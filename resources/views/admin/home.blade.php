3b5c53dd845c312581b79ea3fc26a4e2d4c6e1e0
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-09-13 17:10:32 -0700

    admin interface added

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,140 @@
-<x-app-layout>
-<h1>This is Admin Dashboard</h1>
-</x-app-layout>
+<!doctype html>
+<html lang="en">
+  <head>
+   @include('admin.css')
+  </head>
+  <body>
+    <!-- Page Container -->
+    <!--
+      Available classes for #page-container:
+    
+      GENERIC
+    
+        'remember-theme'                            Remembers active color theme and dark mode between pages using localStorage when set through
+                                                    - Theme helper buttons [data-toggle="theme"],
+                                                    - Layout helper buttons [data-toggle="layout" data-action="dark_mode_[on/off/toggle]"]
+                                                    - ..and/or Dashmix.layout('dark_mode_[on/off/toggle]')
+    
+      SIDEBAR & SIDE OVERLAY
+    
+        'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+        'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+        'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+        'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+        'sidebar-dark'                              Dark themed sidebar
+    
+        'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+        'side-overlay-o'                            Visible Side Overlay by default
+    
+        'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+    
+        'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+    
+      HEADER
+    
+        ''                                          Static Header if no class is added
+        'page-header-fixed'                         Fixed Header
+    
+    
+      FOOTER
+    
+        ''                                          Static Footer if no class is added
+        'page-footer-fixed'                         Fixed Footer (please have in mind that the footer has a specific height when is fixed)
+    
+      HEADER STYLE
+    
+        ''                                          Classic Header style if no class is added
+        'page-header-dark'                          Dark themed Header
+        'page-header-glass'                         Light themed Header with transparency by default
+                                                    (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+        'page-header-glass page-header-dark'         Dark themed Header with transparency by default
+                                                    (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+    
+      MAIN CONTENT LAYOUT
+    
+        ''                                          Full width Main Content if no class is added
+        'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+        'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+        
+      DARK MODE
+    
+        'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
+    -->
+    <div id="page-container" class="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed main-content-narrow">
+      <!-- Side Overlay-->
+      @include('admin.overlay')
+      <!-- END Side Overlay -->
+
+      <!-- Sidebar -->
+      <!--
+        Sidebar Mini Mode - Display Helper classes
+      
+        Adding 'smini-hide' class to an element will make it invisible (opacity: 0) when the sidebar is in mini mode
+        Adding 'smini-show' class to an element will make it visible (opacity: 1) when the sidebar is in mini mode
+          If you would like to disable the transition animation, make sure to also add the 'no-transition' class to your element
+      
+        Adding 'smini-hidden' to an element will hide it when the sidebar is in mini mode
+        Adding 'smini-visible' to an element will show it (display: inline-block) only when the sidebar is in mini mode
+        Adding 'smini-visible-block' to an element will show it (display: block) only when the sidebar is in mini mode
+      -->
+      <!-- Sidebar Start -->
+                @include('admin/sidebar')
+      <!-- END Sidebar -->
+
+        <!-- Header -->
+          <header id="page-header">           
+            <div class="content-header">              
+
+                @include('admin.header')
+                
+            </div>
+        <!-- END Header Content -->
+
+        <!-- Header Search -->
+       @include('admin.search')
+        <!-- END Header Search -->
+
+        <!-- Header Loader -->
+        <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
+        {{-- <div id="page-header-loader" class="overlay-header bg-header-dark">
+          <div class="bg-white-10">
+            <div class="content-header">
+              <div class="w-100 text-center">
+                <i class="fa fa-fw fa-sun fa-spin text-white"></i>
+              </div>
+            </div>
+          </div>
+        </div> --}}
+       
+        <!-- END Header Loader -->
+      </header>
+      <!-- END Header -->
+      
+
+      <!-- Main Container -->
+      <main id="main-container">        
+        <div class="content">
+          <div class="d-md-flex justify-content-md-between align-items-md-center py-3 pt-md-3 pb-md-0 text-center text-md-start">
+            @include('admin.content')
+            
+          </div>
+        </div>        
+      </main>
+      <!-- END Main Container -->
+
+      <!-- Footer -->
+      @include('admin/footer')
+      <!-- END Footer -->
+    </div>
+    <!-- END Page Container -->
+   
+    <script src="admin/assets/js/dashmix.app.min.js"></script>
+
+    <!-- Page JS Plugins -->
+    <script src="admin/assets/js/plugins/chart.js/chart.min.js"></script>
+
+    <!-- Page JS Code -->
+    <script src="admin/assets/js/pages/be_pages_dashboard.min.js"></script>
+  </body>
+</html>
+
